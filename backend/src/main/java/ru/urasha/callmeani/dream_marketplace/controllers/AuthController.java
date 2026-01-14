68dd1d177386f9c2b4f435d8fd3caf9ed672a50a
Vladislav <413022@niuitmo.ru>
2026-01-14 16:24:24 +0300

    feat(logout): added endpoint for logout from account

--- backend/src/main/java/ru/urasha/callmeani/dream_marketplace/controllers/AuthController.java
+++ backend/src/main/java/ru/urasha/callmeani/dream_marketplace/controllers/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.urasha.callmeani.dream_marketplace.config.JwtProperties;
 import ru.urasha.callmeani.dream_marketplace.dto.AuthResponse;
@@ -70,5 +71,16 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.FOUND)
             .header("Location", frontendUrl)
             .build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("access_token", "");
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return ResponseEntity.noContent().build();
     }
 }
